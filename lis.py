acd2921786137e7e819a59f7a475d8c25aaea96a
cuaravind <123807671+cuaravind@users.noreply.github.com>
2023-02-17 11:57:50 +0530

    Update lis.py

--- lis.py
+++ lis.py
@@ -1,7 +1,7 @@
 list=[1,2,3,4,5,6,7,8,9,0]
 list2=["Aravind","Barath","Mani"]
 check=input("enter the char to check:")
-if check in list2:
+# if check in list2:
     print("yes")
 else:
     print("no")
